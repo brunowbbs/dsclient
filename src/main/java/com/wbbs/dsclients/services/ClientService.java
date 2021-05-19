b31cff51d12f13cf43051584bd40cd2ba53a3e93
Wesley Bruno <engwesleybruno@gmail.com>
2021-05-19 09:01:28 -0300

    Clint Delete

--- src/main/java/com/wbbs/dsclients/services/ClientService.java
+++ src/main/java/com/wbbs/dsclients/services/ClientService.java
@@ -3,6 +3,8 @@ package com.wbbs.dsclients.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.wbbs.dsclients.dto.ClientDTO;
 import com.wbbs.dsclients.entities.Client;
 import com.wbbs.dsclients.repositories.ClientRepository;
+import com.wbbs.dsclients.services.exceptions.DatabaseException;
 import com.wbbs.dsclients.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -18,19 +21,19 @@ public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(item -> new ClientDTO(item));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Resource not found"));
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
 		return new ClientDTO(entity);
-		
+
 	}
 
 	@Transactional
@@ -39,17 +42,46 @@ public class ClientService {
 		client = repository.save(client);
 		return new ClientDTO(client);
 	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+
+			Client client = repository.getOne(id);
+			client.setName(dto.getName());
+			client.setCpf(dto.getCpf());
+			client.setBirthDate(dto.getBirthDate());
+			client.setIncome(dto.getIncome());
+			client.setChildren(dto.getChildren());
+			client = repository.save(client);
+			return new ClientDTO(client);
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Resource not found");
+		}
+	}
 	
-	
+	public void delete(Long id) {
+
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found");
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+
 	private Client generateClient(ClientDTO dto) {
+
 		Client client = new Client();
 		client.setName(dto.getName());
 		client.setCpf(dto.getCpf());
 		client.setIncome(dto.getIncome());
 		client.setBirthDate(dto.getBirthDate());
 		client.setChildren(dto.getChildren());
-		
+
 		return client;
+
 	}
 
 }
